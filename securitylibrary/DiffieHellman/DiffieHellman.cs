16d5a589081ed588157b5a182c51dc32ff41f77a
AhmedEsmat1996 <ahmed.esmat@cis.asu.edu.eg>
2019-04-21 00:15:28 +0200

    last milestone
    -diff-hellman
    -elGamal
     key generation
     encryption

--- securitylibrary/DiffieHellman/DiffieHellman.cs
+++ securitylibrary/DiffieHellman/DiffieHellman.cs
@@ -10,7 +10,164 @@ namespace SecurityLibrary.DiffieHellman
     {
         public List<int> GetKeys(int q, int alpha, int xa, int xb)
         {
-            throw new NotImplementedException();
+
+            List<int> key = new List<int>();
+
+
+            var Ya = 0.0;
+
+            var Yb = 0.0;
+
+            if (xa % 2 == 0)
+            {
+
+
+                int i = xa / 2;
+                var a1 = Math.Pow(alpha, 2) % q;
+                var temp = 1.0;
+                for (int j = 0; j < i; j++)
+                {
+                    temp *= a1;
+                    temp %= q;
+                }
+
+                Ya = temp % q;
+
+
+            }
+            else
+            {
+                xa = xa - 1;
+                int i = xa / 2;
+                var aa1 = Math.Pow(alpha, 2) % q;
+                var temp = Math.Pow(alpha, 1) % q;
+
+                for (int j = 0; j < i; j++)
+                {
+                    temp *= aa1;
+                    temp %= q;
+                }
+
+                Ya = temp % q;
+                xa++;
+            }
+
+
+
+
+            if (xb % 2 == 0)
+            {
+
+
+                int i = xb / 2;
+                var a1 = Math.Pow(alpha, 2) % q;
+                var temp = 1.0;
+                for (int j = 0; j < i; j++)
+                {
+                    temp *= a1;
+                    temp %= q;
+                }
+
+                Yb = temp % q;
+
+            }
+            else
+            {
+                xb = xb - 1;
+                int i = xb / 2;
+                var aa1 = Math.Pow(alpha, 2) % q;
+                var temp = Math.Pow(alpha, 1) % q;
+
+                for (int j = 0; j < i; j++)
+                {
+                    temp *= aa1;
+                    temp %= q;
+                }
+
+                Yb = temp % q;
+                xb++;
+            }
+
+
+
+            var Ka = 0.0;
+            if (xa % 2 == 0)
+            {
+
+
+                int i = xa / 2;
+                var x1 = Math.Pow(Yb, 2) % q;
+                var temp = 1.0;
+                for (int j = 0; j < i; j++)
+                {
+                    temp *= x1;
+                    temp %= q;
+                }
+
+                Ka = temp % q;
+
+
+            }
+            else
+            {
+                xa = xa - 1;
+                int i = xa / 2;
+                var x1 = Math.Pow(Yb, 2) % q;
+                var temp = Math.Pow(Yb, 1) % q;
+
+                for (int j = 0; j < i; j++)
+                {
+                    temp *= x1;
+                    temp %= q;
+                }
+
+                Ka = temp % q;
+
+            }
+
+
+            var Kb = 0.0;
+
+
+            if (xb % 2 == 0)
+            {
+
+                int i = xb / 2;
+                var x1 = Math.Pow(Ya, 2) % q;
+                var temp = 1.0;
+                for (int j = 0; j < i; j++)
+                {
+                    temp *= x1;
+                    temp %= q;
+                }
+
+                Kb = temp % q;
+
+            }
+            else
+            {
+                xb = xb - 1;
+                int i = xb / 2;
+                var x1 = Math.Pow(Ya, 2) % q;
+                var temp = Math.Pow(Ya, 1) % q;
+
+                for (int j = 0; j < i; j++)
+                {
+                    temp *= x1;
+                    temp %= q;
+                }
+
+                Kb = temp % q;
+            }
+
+            int ka = (int)Ka;
+            key.Add(ka);
+            int kb = (int)Kb;
+            key.Add(kb);
+
+            return key;
+
+
         }
     }
 }
--- securitylibrary/ElGamal/ELGAMAL.cs
+++ securitylibrary/ElGamal/ELGAMAL.cs
@@ -16,15 +16,71 @@ namespace SecurityLibrary.ElGamal
         /// <param name="y"></param>
         /// <param name="k"></param>
         /// <returns>list[0] = C1, List[1] = C2</returns>
+
+
+
         public List<long> Encrypt(int q, int alpha, int y, int k, int m)
         {
-            throw new NotImplementedException();
+            List<long> res = new List<long> { };
+
+
+            long c1 = kiki(q, alpha, k);
+            int K = kiki(q, y, k);
+            long C2 = (K * m) % q;
+            res.Add(c1);
+            res.Add(C2);
+
+            return res;
 
         }
+
+
+
+        public static int kiki(int q, int alpha, int xb)
+        {
+            var Yb = 0.0;
+
+            if (xb % 2 == 0)
+            {
+
+
+                int i = xb / 2;
+                var a1 = Math.Pow(alpha, 2) % q;
+                var temp = 1.0;
+                for (int j = 0; j < i; j++)
+                {
+                    temp *= a1;
+                    temp %= q;
+                }
+
+                Yb = temp % q;
+
+            }
+            else
+            {
+                xb = xb - 1;
+                int i = xb / 2;
+                var aa1 = Math.Pow(alpha, 2) % q;
+                var temp = Math.Pow(alpha, 1) % q;
+
+                for (int j = 0; j < i; j++)
+                {
+                    temp *= aa1;
+                    temp %= q;
+                }
+
+                Yb = temp % q;
+                xb++;
+            }
+            return (int)Yb;
+        }
+
         public int Decrypt(int c1, int c2, int x, int q)
         {
-            throw new NotImplementedException();
-
+            int K = kiki(q, c1, x);
+            var kinverse = 0 ;
+            int M = (c2 * kinverse) % q;
+            return M;
         }
     }
 }
